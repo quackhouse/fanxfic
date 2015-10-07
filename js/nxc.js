23c9bbcc43a79f30c27a453cb7a071617f18531b
Morgan Neiman <morgan@emotient.com>
2015-10-06 23:55:13 -0700

    grabs frequency data but doesn't do anything with it yet

--- js/nxc.js
+++ js/nxc.js
@@ -1,9 +1,11 @@
 client_id = "4f28b84aa5e37d60c6b6978d99a6c632";
 
 var AudioContext, audio, audioContext, source, analyser, dataArray, sourceBuffer;
+var sourceJs, array;
 var analyserMethod = "getByteFrequencyDomainData";
 var streamURL;
 var playing = false;
+var dataArray;
 
 addEventListeners = function() {
   $('#load-it').click(function(){
@@ -90,12 +92,26 @@ init_stream = function(json_data) {
           $('#playback-speed').prop('value', 1);
           $('.speed').html(1);
           var audioData = request.response;
-          audioContext.decodeAudioData(audioData, function(buffer){
+            audioContext.decodeAudioData(audioData, function(buffer){
             myBuffer = buffer;
+            analyser = audioContext.createAnalyser();
+            sourceJs = audioContext.createScriptProcessor(2048, 1, 1);
+            sourceJs.buffer = buffer;
+            sourceJs.connect(audioContext.destination);
+            analyser.smoothingTimeConstant = 0.6;
+            analyser.fftSize = 512;
+            source.connect(analyser);
+            analyser.connect(sourceJs);
             source.buffer = myBuffer;
             source.playbackRate.value = 1;
             source.connect(audioContext.destination);
+            sourceJs.onaudioprocess = function(e) {
+              array = new Uint8Array(analyser.frequencyBinCount);
+              analyser.getByteFrequencyData(array);
+              startVisuals();
+            };
           });
+
         };
         request.send();
         source.start(0);
@@ -126,7 +142,15 @@ init_stream = function(json_data) {
   }
 };
 
+startVisuals = function() {
 
+  // use array, go thru array k
+  for(var i = 0, len = array.length; i < len; i++) {
+    console.log(array[i]);
+  }
+
+  // window.requestAnimationFrame(startVisuals);
+};
 
 $(document).ready(function(){
   addEventListeners();
